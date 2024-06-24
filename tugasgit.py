88b55e3aedbae0894a5a6b7a2d55576041c132c4
hijirdella <106947192+hijirdella@users.noreply.github.com>
2024-06-24 14:13:18 +0700

    initial proses memasukkan file python dan csv

---
+++ tugasgit.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+file_path = r"C:\Users\hijir\Documents\DE6\dibimbing-belajar-github\username.csv"
+# Alternatively: file_path = "C:/Users/hijir/Documents/DE6/dibimbing-belajar-github/username.csv"
+
+# Read the CSV file
+df = pd.read_csv(file_path)
+
+# Print the first few rows of the dataframe
+print(df.head())
